Added test for backprop, fixed bug in `forward`

src/models/sgns.py
@@ -106,7 +106,7 @@ class MultiEmbeddingSGNS(nn.Module):
     def forward(self, x):
         # x.shape (bs, 2, num_embeddings) should contain indices
         target_indices = x[:, 0, :]
-        context_indices = x[:, 0, :]
+        context_indices = x[:, 1, :]
 
         target_embedding = self.forward_target(target_indices)
         context_embedding = self.forward_context(context_indices)

src/models/test_sgns.py
@@ -15,9 +15,7 @@ def mock_training_data():
             [[0, 0], [3, 3]],
         ]
     )
-    y = torch.tensor(
-            [1., 1.]
-    ).double()
+    y = torch.tensor([1.0, 1.0])
     return X, y
 
 
@@ -71,21 +69,62 @@ def test_forward_correct_shape(mock_model, mock_training_data):
     actual = mock_model.forward(mock_indices)
     assert tuple(actual.shape) == (2,)
 
+
 def test_backprop_works(mock_model, mock_training_data):
-    original_base_target = torch.clone(mock_model.target_embeddings["base"].weight.data)
+    # this test uses a different set of indices to check the specific case of which
+    # embeddings should be affected during backprop
+    X = torch.tensor(
+        [
+            [[1, 2], [0, 2]],
+            [[3, 3], [1, 4]],
+        ]
+    )
+    y = torch.tensor([1.0, 1.0])
+
+    mock_model = MultiEmbeddingSGNS(
+        vocab_size=6, embedding_dim=3, side_info_specs={"s1": 5}
+    )
+
+    # weight matrices before backprop
+    before_base_target = torch.clone(mock_model.target_embeddings["base"].weight.data)
+    before_s1_target = torch.clone(mock_model.target_embeddings["s1"].weight.data)
+    before_base_context = torch.clone(mock_model.context_embeddings["base"].weight.data)
+    before_s1_context = torch.clone(mock_model.context_embeddings["s1"].weight.data)
+
     optimizer = torch.optim.SGD(mock_model.parameters(), lr=0.2)
     loss_fn = nn.BCELoss()
-    X, y = mock_training_data
-
     preds = mock_model(X)
-
     loss = loss_fn(preds, y)
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
+    # weight matrices after backprop
     after_base_target = mock_model.target_embeddings["base"].weight.data
+    after_s1_target = mock_model.target_embeddings["s1"].weight.data
+    after_base_context = mock_model.context_embeddings["base"].weight.data
+    after_s1_context = mock_model.context_embeddings["s1"].weight.data
 
-    assert not torch.equal(original_base_target, after_base_target)
-    print("Woohoo!")
+    # which embedding matrices we expect a change (note lengths are 6 and 5 for base and s1 respectively)
+    base_target_change_expected = torch.tensor([False, True, False, True, False, False])
+    s1_target_change_expected = torch.tensor([False, False, True, True, False])
+    base_context_change_expected = torch.tensor(
+        [True, True, False, False, False, False]
+    )
+    s1_context_change_expected = torch.tensor([False, False, True, False, True])
+
+    row_diff = lambda x, y: (x != y).max(axis=1).values
+
+    assert torch.equal(
+        row_diff(before_base_target, after_base_target), base_target_change_expected
+    )
+    assert torch.equal(
+        row_diff(before_s1_target, after_s1_target), s1_target_change_expected
+    )
+    assert torch.equal(
+        row_diff(before_base_context, after_base_context), base_context_change_expected
+    )
+    assert torch.equal(
+        row_diff(before_s1_context, after_s1_context), s1_context_change_expected
+    )